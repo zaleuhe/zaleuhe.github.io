9b1d31a9f402d797d9ddda7028630fdfad0cc874
Allen Herring <zaleuhe@gmail.com>
2019-11-15 13:53:47 -0600

    Adding Gradiant background
    Its adjustable!

---
+++ ShoppingList/gradiant-background.js
@@ -0,0 +1,27 @@
+// Get items that will be modified
+var css = document.querySelector("h3");
+var colorLeft = document.querySelector('.colorLeft');
+var colorRight = document.querySelector('.colorRight');
+var bodyColor = document.getElementById("gradient");
+var percentage = 50;
+
+colorLeft.addEventListener("input", setGradient);
+colorRight.addEventListener("input", setGradient);
+
+function setGradient(){
+	bodyColor.style.background = "linear-gradient(to right, " + colorLeft.value 
+			+ ", " + colorLeft.value + " " + percentage +"%, " + colorRight.value + ")";
+
+	// Set text content
+	css.textContent = bodyColor.style.background + ";";
+}
+
+
+// SLIDER FUNCTIONALITY
+var slider = document.getElementById("colorRange");
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  percentage = this.value;
+  setGradient();
+}
--- ShoppingList/script.js
+++ ShoppingList/script.js
@@ -1,4 +1,4 @@
-var addButton = document.getElementById("addItem");
+/*var addButton = document.getElementById("addItem");
 var userInput = document.getElementById("userInput");
 var ourList = document.querySelector("ul");
 
@@ -27,7 +27,7 @@ userInput.addEventListener("keypress", function(event){
 	if(hasInput() && event.keyCode === 13){
 		addToList();
 	}
-})
+})*/
 
 /* ALTERNATE METHOD
 
@@ -44,4 +44,77 @@ function addListAfterKeypress(event){
 	}
 }
 userInput.addEventListener("keypress", addListAFterKeypress)
+*/
 
+var input=document.getElementById("userInput");
+var button=document.getElementById("addItem");
+var ul=document.querySelector("ul");
+var listItems=document.getElementsByTagName("li"); 
+
+button.addEventListener("click",addListAfterClick);
+input.addEventListener("keypress",addListAfterKeypress);
+
+function inputLength(){
+	return input.value.length;
+}
+
+function clearInputField() {
+	input.value="";
+}
+
+function createButton (){
+	var btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("X"));
+ 	btn.onclick = removeParent;
+ 	btn.className = "deleteButton";
+ 	return btn;
+}
+
+function createListElement(){
+var li=document.createElement("li");
+	li.appendChild(createButton());
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	
+
+ 	clearInputField();
+}
+
+
+function addListAfterClick(){
+    if (inputLength()>0){
+    	createListElement();
+    }
+}
+
+function isItemInList(addedItem){
+
+}
+
+function addListAfterKeypress(event){
+	  if (inputLength()>0 && event.keyCode===13){
+	  	createListElement();
+	  }
+}
+
+
+
+ul.onclick=function(event){
+	var target=event.target;
+	target.classList.toggle("done");
+}
+
+function listLength(){
+	return listItems.length;
+}
+
+function deleteButton(){
+	var btn=document.createElement("button");
+	btn.appendChild(document.createTextNode("Delete!"));
+	listItems[i].appendChild(btn);
+	btn.onclick=removeParent;
+}
+
+function removeParent(evt){
+	evt.target.parentNode.remove();
+} 
